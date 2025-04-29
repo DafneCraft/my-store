15f1fae2407f911ab1b3d5f21a7011305e95354a
David <dafnehack@gmail.com>
2025-04-28 21:40:35 -0500

    Nuevas Rutas Añadidas

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import LoginUser from "./pages/LoginUser";
+import CreateUser from "./pages/CreateUser";
+import ForgotPassword from "./pages/ForgotPassword";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="src/pages/Home" element={<Home />} />
-        <Route path="/src/pages/About" element={<About />} />
-        <Route path="/src/pages/Store" element={<Store />} />
-        <Route path="/src/pages/Contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/store" element={<Store />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/login" element={<LoginUser />} />
+        <Route exact path="/register" element={<CreateUser />} />
+        <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
